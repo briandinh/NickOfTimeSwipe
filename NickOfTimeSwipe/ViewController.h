9651cec0839a145d7d7cd621143bdc3770b50045
Brian Dinh <bdinh526@gmail.com>
2013-08-13 16:29:45 -0500

    Initial Commit

---
+++ NickOfTimeSwipe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NickOfTimeSwipe
+//
+//  Created by Brian Dinh on 8/13/13.
+//  Copyright (c) 2013 Brian Dinh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ NickOfTimeSwipe/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NickOfTimeSwipe
+//
+//  Created by Brian Dinh on 8/13/13.
+//  Copyright (c) 2013 Brian Dinh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
