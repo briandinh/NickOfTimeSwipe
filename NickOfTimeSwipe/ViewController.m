5174b592a6d52c575c24404e3b025db0c17290c4
Brian Dinh <bdinh526@gmail.com>
2013-08-13 22:03:29 -0500

    Added swipe gesture recognizers, created array with commands

--- NickOfTimeSwipe/ViewController.m
+++ NickOfTimeSwipe/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    NSArray *commands;
+    int index;
+}
 
 @end
 
@@ -17,13 +20,53 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
+    [self addSwipeGestures];
+    commands = [NSArray arrayWithObjects:@"Swipe left",@"Swipe right",@"Swipe up",@"Swipe down", nil];
+    index = arc4random() % 4;
 
+}
 - (void)didReceiveMemoryWarning
 {
+    
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)addSwipeGestures{
+    UISwipeGestureRecognizer* swipeUpGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeUpFrom:)];
+    swipeUpGestureRecognizer.direction = UISwipeGestureRecognizerDirectionUp;
+    
+    UISwipeGestureRecognizer* swipeDownGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeDownFrom:)];
+    swipeDownGestureRecognizer.direction = UISwipeGestureRecognizerDirectionDown;
+    
+    UISwipeGestureRecognizer* swipeLeftGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeLeftFrom:)];
+    swipeLeftGestureRecognizer.direction = UISwipeGestureRecognizerDirectionLeft;
+    
+    UISwipeGestureRecognizer* swipeRightGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeRightFrom:)];
+    swipeRightGestureRecognizer.direction = UISwipeGestureRecognizerDirectionRight;
+    
+    [self.view addGestureRecognizer:swipeUpGestureRecognizer];
+    [self.view addGestureRecognizer:swipeDownGestureRecognizer];
+    [self.view addGestureRecognizer:swipeLeftGestureRecognizer];
+    [self.view addGestureRecognizer:swipeRightGestureRecognizer];
+}
+
+- (void)handleSwipeUpFrom:(UIGestureRecognizer*)recognizer {
+    NSLog(@"swiped up");
+}
+
+- (void)handleSwipeDownFrom:(UIGestureRecognizer*)recognizer {
+    NSLog(@"swiped down");
+}
+
+- (void)handleSwipeLeftFrom:(UIGestureRecognizer*)recognizer {
+    NSLog(@"swiped left");
+}
+
+- (void)handleSwipeRightFrom:(UIGestureRecognizer*)recognizer {
+    NSLog(@"swiped right");
+}
+
+
 
 @end
